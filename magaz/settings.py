593923170b28bca1373785266e8bcab6a1f7b2db
Mark <anwardo13@gmail.com>
2018-02-17 20:39:08 +0300

    Update settings.py

--- magaz/settings.py
+++ magaz/settings.py
@@ -65,8 +65,8 @@ CKEDITOR_BROWSE_SHOW_DIRS = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "elmarket.minsk@gmail.com"
-EMAIL_HOST_PASSWORD = "0192837465z"
+EMAIL_HOST_USER = "email@email.com"
+EMAIL_HOST_PASSWORD = "password"
 EMAIL_USE_TLS = True
 
 SERVER_EMAIL = EMAIL_HOST_USER
